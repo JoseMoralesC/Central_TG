fb03c7816f43a8a8c5fbb96c224b8b350543d6c7
Carlos Mata <carlosmatafallas13@gmail.com>
2026-06-04 00:36:46 -0600

    Primeras lineas en  sockets/Socket TCP

--- java_proveedor/src/sockets/SocketTCP.java
+++ java_proveedor/src/sockets/SocketTCP.java
@@ -12,6 +12,33 @@ import java.net.Socket;
 //Clase Socket
 public class SocketTCP
 {
+    public static void main(String[] args)
+    {
+        try
+    {
+        //Servidor TCP escuchando en el puerto 6000
+        ServerSocket serverSocket = new ServerSocket(6000);
+        System.out.println("Servidor TCP escuchando...");
+
+        while (true)
+        {
+            //Aceptar una conexión entrante
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
+
+            //Cerrar la conexión con el cliente
+            clientSocket.close();
+            serverSocket.close();
+
+            break; // Salir del bucle después de manejar una conexión (opcional, dependiendo de si quieres manejar múltiples conexiones)
+        }
+    }
+
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    }
     
 }
 //Fin de la clase Socket
