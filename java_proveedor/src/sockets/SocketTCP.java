efec6834a2b6914cde881f1219b28c4a9080fb63
Carlos Mata <carlosmatafallas13@gmail.com>
2026-06-04 00:31:00 -0600

    Clase Cliente en models

--- java_proveedor/src/sockets/SocketTCP.java
+++ java_proveedor/src/sockets/SocketTCP.java
@@ -5,6 +5,7 @@ package java_proveedor.src.sockets;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
