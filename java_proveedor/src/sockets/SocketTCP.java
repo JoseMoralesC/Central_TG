4e224aa873fd77390a801604486c6d55da538f97
Carlos Mata <carlosmatafallas13@gmail.com>
2026-06-03 23:13:48 -0600

    Agregando los imports necesarios a la clase SocketTCP

---
+++ java_proveedor/src/sockets/SocketTCP.java
@@ -0,0 +1,16 @@
+//Package
+package java_proveedor.src.sockets;
+
+//Imports
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//Clase Socket
+public class SocketTCP
+{
+    
+}
+//Fin de la clase Socket
