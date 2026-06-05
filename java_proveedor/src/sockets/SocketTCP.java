8505b1372f115d6e6a886b1ba2c7eb1bcdd5f5af
Carlos Mata <carlosmatafallas13@gmail.com>
2026-06-04 19:48:05 -0600

    Agregado de los documentos en Configuracion

--- java_proveedor/src/sockets/SocketTCP.java
+++ java_proveedor/src/sockets/SocketTCP.java
@@ -2,9 +2,6 @@
 package java_proveedor.src.sockets;
 
 //Imports
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
 import java.net.ServerSocket;
 import java.net.Socket;
